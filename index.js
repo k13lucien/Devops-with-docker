14f192dd34f2e5459383b462035e0ee6a2102d4e
Lucien Kiemde <lucienkiemde4@gmail.com>
2026-01-09 15:32:46 +0000

    adding compose.yaml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World from my app!</h1>')
+  res.send('<h1>Hello World! The pipeline is totaly fine</h1>')
 })
 
 const PORT = 8080
