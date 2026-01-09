acc534782ccb8fdf27303c8db7eb23b6051b6a8f
Lucien Kiemde <lucienkiemde4@gmail.com>
2026-01-09 15:19:57 +0000

    editinng

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World from my express app!</h1>')
+  res.send('<h1>Hello World from my app!</h1>')
 })
 
 const PORT = 8080
